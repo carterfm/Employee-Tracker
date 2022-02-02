5c5cd422d81b7d303ac6989c5b9d44b25f1d9f2d
carterfm <carterf.morfitt@gmail.com>
2022-02-01 17:29:22 -0800

    Imported required packages and built schema file for database

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department_table (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role_table (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department_table(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee_table (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role_table(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee_table(id) ON DELETE SET NULL
+);
+
+--potentially change some of these these SET NULLs to cascade later depending on feedback from prof + TAs
